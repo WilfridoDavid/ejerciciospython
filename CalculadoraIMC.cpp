434b06287b61a39bcb9b8a7562b2175820aad34a
WilfridoDavid <wilfridodavid1969@gmail.com>
2025-06-03 13:46:11 -0600

    Update CalculadoraIMC.cpp

--- CalculadoraIMC.cpp
+++ CalculadoraIMC.cpp
@@ -4,7 +4,7 @@ using namespace std;
 
 main()
 {
-    
+    cout<<"hola mundo";
     setlocale(LC_ALL, "");
     cout<<"Hola mundo"
     cout<<"PROGRAMA QUE CALCULA EL INDICE DE MASA CORPORAL" <<endl;
